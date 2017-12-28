70e473c0b336bd6798c66ef0b288a27e1bd04fdc
Bharat Dwarkani <bharatdwarkanim@syncfusion.com>
2017-12-29 01:01:04 +0530

    Deleted bin, obj Included home page Added some features in story page grid

--- TaskPlanner/Controllers/HomeController.cs
+++ TaskPlanner/Controllers/HomeController.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using TaskPlanner.Models;
 
 namespace TaskPlanner.Controllers
 {
+    [AllowAnonymous]
     public class HomeController : Controller
     {
         public IActionResult Index()
