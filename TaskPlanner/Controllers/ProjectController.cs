d09ecd7c11e1d6722a09c6a9ac51c67af2f0c6b6
Bharat Dwarkani <bharatdwarkanim@syncfusion.com>
2017-12-28 11:00:41 +0530

    Initial project committed

---
+++ TaskPlanner/Controllers/ProjectController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TaskPlanner.Controllers
+{
+    public class ProjectController : Controller
+    { 
+
+    }
+}
